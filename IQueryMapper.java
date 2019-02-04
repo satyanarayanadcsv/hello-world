1cd17a6f5114406322ac8c8b5a652edefc960877
satyanarayanadcsv <35227100+satyanarayanadcsv@users.noreply.github.com>
2019-02-04 18:12:23 +0530

    Update IQueryMapper.java

--- IQueryMapper.java
+++ IQueryMapper.java
@@ -6,5 +6,10 @@ public interface IQueryMapper {
 	String GET_FLIGHTS = "select * from FLIGHTINFORMATION where dep_city=? and arr_city=?";
 	String GET_FIRST_SEAT_FARE = "select firstseatfare from FLIGHTINFORMATION where flightno=?";
 	String GET_BUS_SEAT_FARE = "select busseatsfare from FLIGHTINFORMATION where flightno=?";
+	String GET_BOOKINGID = "select air_booking_id.currval from dual";
+	String GET_FIRST_SEATS = "select firstseats from flightinformation where flightno=?";
+	String UPDATE_FIRST_SEATS = "update flightinformation set firstseats=? where flightno=?";
+	String GET_BUS_SEATS = "select busseats from flightinformation where flightno=?";
+	String UPDATE_BUS_SEATS = "update flightinformation set busseats=? where flightno=?";
 	
 }
